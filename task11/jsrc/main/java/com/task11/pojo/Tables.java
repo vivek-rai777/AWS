2890aa1e20e454264bcc0d96154e4c5067c899b2
vivek-rai777 <163520452+vivek-rai777@users.noreply.github.com>
2024-05-20 16:52:40 +0530

    Swagger Ui

---
+++ task11/jsrc/main/java/com/task11/pojo/Tables.java
@@ -0,0 +1,57 @@
+package com.task11.pojo;
+
+import com.google.gson.annotations.SerializedName;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+public class Tables {
+    private int id;
+    private int number;
+    private int places;
+    @SerializedName("vip")
+    private boolean isVip;
+    private int minOrder;
+
+    @DynamoDbPartitionKey
+    public int getId() {
+        return id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public boolean isVip() {
+        return isVip;
+    }
+
+    public int getMinOrder() {
+        return minOrder;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setPlaces(int places) {
+        this.places = places;
+    }
+
+    public void setVip(boolean vip) {
+        isVip = vip;
+    }
+
+    public void setMinOrder(int minOrder) {
+        this.minOrder = minOrder;
+    }
+}
+ 
