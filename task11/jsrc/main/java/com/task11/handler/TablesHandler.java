502e43312a2a7e6a33391cffb354484190b5eca7
vivek-rai777 <163520452+vivek-rai777@users.noreply.github.com>
2024-05-22 12:49:23 +0530

    Open API Specification, Documenting

---
+++ task11/jsrc/main/java/com/task11/handler/TablesHandler.java
@@ -0,0 +1,66 @@
+package com.task11.handler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.task11.pojo.Tables;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TablesHandler {
+    private final DynamoDbTable<Tables> tablesDynamoDbTable;
+
+    public TablesHandler(DynamoDbTable<Tables> tablesDynamoDbTable) {
+        this.tablesDynamoDbTable = tablesDynamoDbTable;
+    }
+
+    Map<String,String> headers = new HashMap<>(Map.of( "Access-Control-Allow-Headers","Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin","'*'",
+            "Access-Control-Allow-Methods", "'*'",
+            "Accept-Version","'*'"));
+    public APIGatewayProxyResponseEvent getAllTables() {
+
+        List<Tables> tables = tablesDynamoDbTable.scan().items().stream().collect(Collectors.toList());
+
+        Map<String, List<Tables>> map = new HashMap<>();
+        map.put("tables", tables);
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        response.setBody(new Gson().toJson(map));
+        response.setHeaders(headers);
+        return response;
+    }
+
+    public APIGatewayProxyResponseEvent postTables(APIGatewayProxyRequestEvent request) {
+
+        Gson gson = new Gson();
+        Tables tableItem = gson.fromJson(request.getBody(), Tables.class);
+
+        tablesDynamoDbTable.putItem(tableItem);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        response.setBody("{\"id\": " + tableItem.getId() + "}");
+        response.setHeaders(headers);
+        return response;
+    }
+
+    public APIGatewayProxyResponseEvent getTables(APIGatewayProxyRequestEvent event) {
+        int tableId = Integer.parseInt(event.getPathParameters().get("tableId"));
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+        Optional<Tables> table = tablesDynamoDbTable.scan().items().stream().filter(x -> x.getId()==tableId).findFirst();
+
+        if(table.isPresent()) {
+            response.setStatusCode(200);
+            response.setBody(new Gson().toJson(table.get()));
+        }
+        else {
+            response.setStatusCode(400);
+        }
+        response.setHeaders(headers);
+        return response;
+    }
+}
